24e0132adb5e786bd44e71975d84426d7fe73f53
Lorenzo <lorenzo.mendoza@gmail.com>
2020-03-20 16:59:38 -0600

    Subo algunos cambios

--- src/main/java/com/lorenzo/soap/service/SOAPConnector.java
+++ src/main/java/com/lorenzo/soap/service/SOAPConnector.java
@@ -1,8 +1,19 @@
 package com.lorenzo.soap.service;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Table;
+import javax.persistence.metamodel.EntityType;
+import javax.persistence.metamodel.Metamodel;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 
 public class SOAPConnector extends WebServiceGatewaySupport {
+	
+	private static final String CLASS_MODEL_LOCATION = "mx.com.credifiel.WSECredifiel.model.";
+	
+	@Autowired
+	private EntityManager em;
 	
 	public Object callWebService( String urlService, Object request) throws Exception {
 		try {
@@ -11,6 +22,27 @@ public class SOAPConnector extends WebServiceGatewaySupport {
 			
 		} catch( Exception err ) {
 			throw new Exception( "Error al consumir el servicio: " + urlService );
+		}
+	}
+	
+	// Retreive table name using persist objet mapping name
+	public String getTableName( String cataloType ) throws Exception {
+		try {
+			// Full path is necessary
+			Class<?> entityClass = Class.forName( CLASS_MODEL_LOCATION + cataloType.trim() );
+			
+			Metamodel meta = em.getMetamodel();
+		    
+			EntityType<?> entityType = meta.entity( entityClass );
+		    
+		    Table t = entityClass.getAnnotation(Table.class);
+		    
+		    String tableName = (t == null) ? entityType.getName().toUpperCase() : t.name();
+		    
+		    return tableName;
+		    
+		} catch( Exception err ) {
+			throw new Exception( "Error al recuperar el nombre de la tabla." );
 		}
 	}
 }
--- src/main/java/com/lorenzo/soapconsumer/SoapconsumerApplication.java
+++ src/main/java/com/lorenzo/soapconsumer/SoapconsumerApplication.java
@@ -2,12 +2,34 @@ package com.lorenzo.soapconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableAsync
 public class SoapconsumerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SoapconsumerApplication.class, args);
 	}
-
+	
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+	
+	// https://dzone.com/articles/spring-boot-creating-asynchronous-methods-using-as
+	@Bean("threadPoolTaskExecutor")
+	public TaskExecutor getAsyncExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(20);
+		executor.setMaxPoolSize(1000);
+		executor.setWaitForTasksToCompleteOnShutdown(true);
+		executor.setThreadNamePrefix("wsacredifiel-async-");
+		return executor;
+	}
 }
